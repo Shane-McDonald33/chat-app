3cee7b2cd2954f142ea0df579dbe6cc58666f205
aydenapollo <mcdonaldshane33@gmail.com>
2021-09-29 23:15:28 -0700

    adding dependencies

--- server/index.js
+++ server/index.js
@@ -1,22 +1,30 @@
-const express = require('express');
+const app = require('express')();
+
+
+const http = require('http')
+.createServer(app)
 
 const PORT = process.env.PORT || 3000
+const io = require('socket.io')(http)
 
-const app = express();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`)
 });
+
+io.on('connection', (socket) => {
+    console.log('New Client Connected')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
